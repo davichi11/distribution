fb54230f8370153ef8b1587f56840ebd9880f72a
胡春亮 <huchunliang@gaoxinzb.com>
2018-10-29 17:43:21 +0800

    MQ加入ack

--- src/main/kotlin/com/distribution/queue/LevelUpReceiver.kt
+++ src/main/kotlin/com/distribution/queue/LevelUpReceiver.kt
@@ -2,13 +2,16 @@ package com.distribution.queue
 
 import com.alibaba.fastjson.JSON
 import com.distribution.modules.dis.service.DisMemberInfoService
+import com.rabbitmq.client.Channel
 import org.apache.commons.lang.StringUtils
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.amqp.core.Message
 import org.springframework.amqp.rabbit.annotation.RabbitHandler
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import org.springframework.transaction.interceptor.TransactionAspectSupport
 
 /**
  * @author ChunLiang Hu
@@ -26,7 +29,7 @@ class LevelUpReceiver {
     private lateinit var disMemberInfoService: DisMemberInfoService
 
     @RabbitHandler
-    fun process(msg: String) {
+    fun process(msg: String, channel: Channel, message: Message) {
         log.info("进入会员升级队列,数据为{}", msg)
         if (StringUtils.isBlank(msg)) {
             log.error("消息为空")
@@ -36,8 +39,12 @@ class LevelUpReceiver {
             val memberObject = JSON.parseObject(msg)
             val memberInfo = disMemberInfoService.queryObject(memberObject.getString("id"))
             memberInfo?.let { disMemberInfoService.levelUp(it) }
+            channel.basicAck(message.messageProperties.deliveryTag, false)
         } catch (e: Exception) {
             log.error("会员升级异常", e)
+            //手动回滚事务
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly()
+            channel.basicReject(message.messageProperties.deliveryTag, true)
         }
     }
 
--- src/main/kotlin/com/distribution/queue/NotifySender.kt
+++ src/main/kotlin/com/distribution/queue/NotifySender.kt
@@ -1,6 +1,11 @@
 package com.distribution.queue
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.amqp.core.Message
 import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.amqp.rabbit.support.CorrelationData
+import org.springframework.beans.factory.InitializingBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
@@ -15,11 +20,53 @@ import org.springframework.stereotype.Component
  * @create 2018/1/17-14:11
  */
 @Component
-class NotifySender {
+class NotifySender : RabbitTemplate.ReturnCallback, RabbitTemplate.ConfirmCallback, InitializingBean {
+    private val log: Logger = LoggerFactory.getLogger(NotifySender::class.java)
     @Autowired
-    private lateinit var rabbitTemplate: RabbitTemplate
+    private lateinit var rabbitAmqpTemplate: RabbitTemplate
+
+    /**
+     * Returned message callback.
+     * @param message the returned message.
+     * @param replyCode the reply code.
+     * @param replyText the reply text.
+     * @param exchange the exchange.
+     * @param routingKey the routing key.
+     */
+    override fun returnedMessage(message: Message?, replyCode: Int, replyText: String?, exchange: String?, routingKey: String?) {
+        log.info("发送房源数据到B端,数据为:$message")
+    }
+
+    /**
+     * Confirmation callback.
+     * @param correlationData correlation data for the callback.
+     * @param ack true for ack, false for nack
+     * @param cause An optional cause, for nack, when available, otherwise null.
+     */
+    override fun confirm(correlationData: CorrelationData?, ack: Boolean, cause: String?) {
+        if (ack) {
+            log.info("消息发送成功")
+        } else {
+            log.error("消息发送失败")
+        }
+    }
+
+    /**
+     * Invoked by a BeanFactory after it has set all bean properties supplied
+     * (and satisfied BeanFactoryAware and ApplicationContextAware).
+     *
+     * This method allows the bean instance to perform initialization only
+     * possible when all bean properties have been set and to throw an
+     * exception in the event of misconfiguration.
+     * @throws Exception in the event of misconfiguration (such
+     * as failure to set an essential property) or if initialization fails.
+     */
+    override fun afterPropertiesSet() {
+        rabbitAmqpTemplate.setConfirmCallback(this)
+        rabbitAmqpTemplate.setReturnCallback(this)
+    }
 
     fun send(context: String) {
-        rabbitTemplate.convertAndSend("notify", context)
+        rabbitAmqpTemplate.convertAndSend("notify", context)
     }
 }
